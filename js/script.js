a1adba8caa22929400c13ffd4267459d155e1265
Millah06 <abdullahialiyugarba14@gmail.com>
2026-02-19 13:57:21 +0000

    'initial

---
+++ js/script.js
@@ -0,0 +1,3 @@
+
+const menuToggle=document.querySelector('.menu-toggle');const navLinks=document.querySelector('nav ul');
+menuToggle.addEventListener('click',()=>{navLinks.classList.toggle('show');});
